3baa60a3e32de25ca403e0c885ffd381e66613de
hades42 <nguyenvannguyen.oc@gmail.com>
2021-02-23 09:49:01 +1100

    Adding Checkout route to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import Layout from "./Components/Layout/Layout"
-import BurgerBuider from "./Container/BurgurBuilder/BurgerBuilder"
-import Checkout from "./Container/Checkout/Checkout"
+import Layout from "./Components/Layout/Layout";
+import BurgerBuider from "./Container/BurgurBuilder/BurgerBuilder";
+import Checkout from "./Container/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuider></BurgerBuider>
-        <Checkout></Checkout>
+        <Switch>
+          <Route path="/checkout" component={Checkout}></Route>
+          <Route path="/" exact component={BurgerBuider}></Route>
+        </Switch>
       </Layout>
     </div>
   );
